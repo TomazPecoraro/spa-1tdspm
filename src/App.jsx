32b13b4d5356cd84cfcdda86d63444e9fdb68339
TomazPecoraro <tomazpecoraro0@gmail.com>
2023-08-28 11:58:13 -0300

    Iniciando componetes no Boyler-plate Vite+React

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Cabecalho from "./componentes/Cabecalho"
+
+export default function App(){
+
+  {/* Isso é um comentário */}
+  return (
+    <>
+      <div class Name="container">
+
+      <Cabecalho/>
+
+      <section>
+        <div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus eveniet qui in rem! Magni, recusandae, laudantium nam dignissimos quos ad rem similique culpa vel debitis perspiciatis eius dolores ratione aspernatur!
+          </p>
+
+        </div>
+      </section>
+      </div>
+
+      <footer>
+        <div>
+          <ul>
+
+          </ul>
+        </div>
+      </footer>
+    </>
+  )
+}
---
+++ src/componentes/Cabecalho.jsx
@@ -0,0 +1,18 @@
+export default function App(){
+
+    return (
+      <>
+        <div>
+        <header>
+          <h1> Vite + React
+            <ul>
+              <li><a href="#">Item - 1</a></li>
+              <li><a href="#">Item - 2</a></li>
+              <li><a href="#">Item - 3</a></li>
+            </ul>
+          </h1>
+        </header>
+        </div>
+      </>
+    )
+  }
